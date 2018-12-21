1a1c7c41adf0df47a55e920e16d42e81e7e70618
mallond <mallond@gmail.com>
2018-12-21 15:39:41 -0800

    add some heap checking and transaction counts.

--- expressServer.js
+++ expressServer.js
@@ -2,15 +2,19 @@
 const express = require('express');
 const kue = require('kue');
 const uuidv1 = require('uuid/v1');
-
+const Redis = require('ioredis');
+const redis = new Redis();
+const client = new Redis();
 
 const app = express();
 const queue = kue.createQueue();
-
+let counter = 0;
+const beginDateTime = new Date();
+let transactionsRequested = null;
 let jobStack = [];
 
+// Create Jobs lots of them
 const createJob = (jobName, sequence)=>{
-
   const title = uuidv1();
   const job = queue.create('oneMillion', {
     title: title,
@@ -23,6 +27,8 @@ const createJob = (jobName, sequence)=>{
           console.log('error');
           return;
         }
+
+        // Keep Jobs on a Stack until Completed
         jobStack.push(job);
 
         job.on('complete', (result) => {
@@ -47,13 +53,27 @@ const createJob = (jobName, sequence)=>{
 app.get('/', (req, res) => res.send('Example: http://localhost:3001/create?jobName=tada&transactions=11'));
 app.get('/create', (req, res) => {
 
-  console.log(req.query)
+  console.log('Request Query Values: ',req.query)
   const transactions = parseInt(req.query.transactions);
+  transactionsRequested =transactions;
 
+  // Create Simulated Jobs
   for (let i=0; i<transactions; i++) {
     createJob(req.query.jobName, i);
   }
 
+  // JOBQ Setup and Subscribe
+  redis.subscribe(req.query.jobName, function (err, count) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  redis.on('message', function(channel, message) {
+    counter = message;
+  });
+  client.set(req.query.jobName,0);
+
+  // Send job message back to user
   res.send(`Job Created and Submitted to stack ${jobStack.length} Transactions: ${req.query.jobName}`);
 
 });
@@ -66,7 +86,21 @@ setInterval(()=>{
   for (let key in used) {
     console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
   }
+  console.log('job count ', counter)
 
-},5000);
+  if (counter===transactionsRequested) {
+    console.log('JOB Done!');
+    const endTimeDate = new Date();
+    console.log(endTimeDate-beginDateTime)
+    process.exit();
+  }
+
+
+},1000);
+
+
+
+
+
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'));
--- workerInstance.js
+++ workerInstance.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
 const kue = require('kue');
+var Redis = require('ioredis');
+
+var client = new Redis();
 
 const queue = kue.createQueue();
 console.log('Worker Connected and Waiting to process job: Queue is oneMillion');
@@ -12,6 +15,17 @@ queue.process('oneMillion', (job, done) => {
 
   //done(null, `Title: ${job.data.title} Job: ${job.data.jobName} Sequence: ${job.data.sequence} completed`);
   done(null, {jobName: job.data.jobName, title:job.data.title, sequence:job.data.sequence})
+
+
+
+  client.incr(job.data.jobName, function(err, reply) {
+    //console.log(reply); // 11
+  });
+
+  client.get('tada').then((value)=>{
+    client.publish('tada', value);
+  })
+
 
 
 

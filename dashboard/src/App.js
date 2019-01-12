8c977925eac13f623168338d6ea57e8b10ca694b
mallond <mallond@gmail.com>
2019-01-12 01:34:02 -0600

    reactify

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -7,30 +7,26 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
 
-          <div className="App-gauge">
 
-            <div className="App-gauge-job-tittle">
-              <div className="App-gauge-job">Job:One Million<div className="App-gauge-job-description">Prototype Job Run</div></div>
+          <div className="App-iphone8">
+            <div className="App-atomic App-logo"></div>
+            <div className="App-input">
+              <div className="App-gauge-run">
+                <div>Number of Objects: <input type="text"/></div>
+                <div>Number of Workers: <input type="text"/></div>
+                <div><input type="button"></input></div>
+                <br/>
+                <div>State: Loading...</div>
+                <div>Percentage: 80 %</div>
+                <div>Object Target: 1,000,000.00</div>
+                <div>Object Count: 100,000</div>
+                <br/>
+                <div>Server Count: 3</div>
+                <div>CPU: 33% Memory: 33%</div>
+              </div>
             </div>
-
-            <div className="App-gauge-run">
-              <div>Number of Objects: <input type="text"/></div>
-              <div>Number of Workers: <input type="text"/></div>
-              <div><input type="button"></input></div>
-            </div>
-
-            <div className="App-gauge-chart">
-              <div className="App-gauge-ellipse-percentage">80%</div>
-              <div className="App-gauge-ellipse-percentage"></div>
-              <div className="App-gauge-ellipse-full"></div>
-            </div>
-
-            <div className="App-gauge-metrics">
-              <div className="App-gauge-runtime">Runtime: 3:35 Minutes<div className="App-gauge-m">Memory:6GB CPU:33 </div></div>
-
-            </div>
+            <div className="App-workarea"></div>
           </div>
 
         </header>
